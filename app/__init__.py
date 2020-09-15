6661006f525a8053ee9ebec2ee99e5c7551adde7
harsha <harshav92@github.com>
2020-09-15 23:15:52 +0530

    main app

---
+++ app/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import Blueprint
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:Prjreddy_64@localhost/tasks"
+
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
+
+from app import route
+
+
